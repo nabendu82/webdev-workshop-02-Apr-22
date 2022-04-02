b27d6cb67b185d32415f15f0aae046ec1c7224a3
nabendu82 <nabendu.biswas@gmail.com>
2022-04-02 22:06:07 +0530

    Final commit

---
+++ CSS-Basics/main.js
@@ -0,0 +1,18 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const ageInput = document.querySelector('#age');
+const msgInput = document.querySelector('#message');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    const li = document.createElement('li');
+    const text = document.createTextNode(`${nameInput.value} : ${ageInput.value} : ${msgInput.value}`);
+    li.appendChild(text);
+    userList.appendChild(li);
+    nameInput.value = '';
+    ageInput.value = '';
+    msgInput.value = '';
+}
